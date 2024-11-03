7bf23eb66b21db04f6757ae5ca7b645ab2a80eec
Noah P <noah.pa@yahoo.com>
2024-11-03 11:22:36 +0000

    setup

---
+++ im-noah-chat/app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { Open_Sans } from "next/font/google";
+import "./globals.css";
+
+const openSans = Open_Sans({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700", "800"],
+});
+
+export const metadata: Metadata = {
+  title: "ImNoahChat",
+  description: "Chat app by ImNoah",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${openSans.className} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
